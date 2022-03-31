655f498d9b17a47a1468cc2b903ec51172947655
whoisltd <tiendat.le129@gmail.com>
2022-03-31 15:50:06 +0700

    change name file .pbxt to .pbtxt

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 text_detection = {
-    'path_to_model': '/home/whoisltd/detect/src/detector/config_text_detection/model.tflite',
-    'path_to_labels': '/home/whoisltd/detect/src/detector/config_text_detection/label_map.pbxt',
+    'path_to_model': '/home/whoisltd/detect/training/exported-models/my_model/saved_model',
+    'path_to_labels': '/home/whoisltd/detect/src/detector/config_text_detection/label_map.pbtxt',
     'nms_ths': 0.2,
     'score_ths': 0.2
 
